10a1b86b41e82bbe4fa2967dad68a93c8ee05c19
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-01-18 21:49:54 +0800

    Revert "18 Januari 2022 9.45"
    This reverts commit 55e214fdc87883c98d202f91d18083fecce8cd8f.

--- Restaurant Silvana/Laporan.cs
+++ Restaurant Silvana/Laporan.cs
@@ -291,8 +291,7 @@ namespace Restaurant_Silvana
                         reportViewer1.LocalReport.SetParameters(ParameterTanggal);
                     }
 
-                    MessageBox.Show("Test");
-                    MessageBox.Show("Halo halo bandungggg");
+                    
                 }
             }
 
